e069dcade0e0d502bcc8026869e53c629002c415
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-08 07:52:04 -0400

    Progess question mark

---
+++ function_code.py
@@ -0,0 +1,33 @@
+
+with open("u.data", encoding="latin1") as movieratings:
+    ratings_table = movieratings.read()
+# user id | item id | rating | timestamp
+ratings_list = ratings_table.split('\n')
+ratingsrow_list = []
+for row in ratings_list:
+    row_list = row.split('\t')
+    if row_list[0] != '':
+        ratingsrow_list.append(row_list)
+
+with open("u.item", encoding="latin1") as movieinformation:
+    information_table = movieinformation.read()
+# movie id|title|releasedate|videodate|imdb|un|act|adv|ani|child|com|crim|doc|drama|fant|
+# |f-noir|horror|mus|mys|rom|syfy|thril|war|west
+information_list = information_table.split('\n')
+inforow_list = []
+for row in information_list:
+    row_list = row.split('|')
+    if row_list[0] != '':
+        inforow_list.append(row_list)
+print(inforow_list)
+
+with open("u.user", encoding="latin1") as userinformation:
+    user_table = userinformation.read()
+# userid | age | gender | occupation | zip code
+user_list = user_table.split('\n')
+finaluser_list = []
+for row in user_list:
+    row_list = row.split('|')
+    if row_list[0] != '':
+        finaluser_list.append(row_list)
+raise Exception("Process void of error")
--- movierating/models.py
+++ movierating/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class Movie(models.Model):
-    movie_id = models.IntegerField()
-    movie_name = models.CharField(max_length=30)
-    release_date = models.CharField(max_length=15)
+    movie_id = models.IntegerField(default=0)
+    movie_name = models.CharField(max_length=30, default='name')
+    release_date = models.CharField(max_length=15, default='date')
     video_release = models.CharField(max_length=20, null=True)
-    imbd_link = models.CharField(max_length=120)
+    imbd_link = models.CharField(max_length=100, default="IMD")
     unknown = models.IntegerField(default=0)
     action = models.IntegerField(default=0)
     adventure = models.IntegerField(default=0)
@@ -23,17 +23,25 @@ class Movie(models.Model):
     war = models.IntegerField(default=0)
     western = models.IntegerField(default=0)
 
+    def __str__(self):
+        movie_str = "Movie name: {}".format(self.movie_name)
+        return(movie_str)
+
 
 class Rater(models.Model):
-    userid = models.IntegerField()
-    age = models.IntegerField()
-    gender = models.CharField(max_length=1)
-    occupation = models.CharField(max_length=20)
-    zipcode = models.IntegerField()
+    userid = models.IntegerField(default=0)
+    age = models.IntegerField(default=0)
+    gender = models.CharField(max_length=1, default="g")
+    occupation = models.CharField(max_length=20, default="occupation")
+    zipcode = models.IntegerField(default=0)
+
+    def __str__(self):
+        rater_str = "{}, {}, {}".format(self.userid, self.age, self.gender)
+        return rater_str
 
 
 class Rating(models.Model):
     userid = models.ForeignKey(Rater)
     movieid = models.ForeignKey(Movie)
-    rating = models.IntegerField()
-    timestamp = models.IntegerField()
+    rating = models.IntegerField(default=1)
+    timestamp = models.IntegerField(default=1)
